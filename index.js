cbd9c46251789dd9110f46f3f46681848553c545
Knoxx04 <onyiknoxx3@gmail.com>
2022-12-20 12:49:22 +0300

    new html,css,and declaration of variables in index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://thecocktaildb.com/api/json/v1/1/search.php?s=";
+let userInput = document.getElementById("user-inp").value;
+if(userInput.length == 0){
+    result.innerHTML=`<h3 class="msg">Theiput field cannot be empty </h3>`;
+}
+else{
+    fetch(url + userInp).then(response => response.json())
+    .then(data => )
+}
